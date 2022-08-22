7098bbb21483dccf9b3234c40cc9436bfe4ecdf8
WillGolden80742 <william80742@gmail.com>
2022-08-22 00:14:44 -0300

    adicionando remoção de imagem de cache

--- Controller/FileController.php
+++ Controller/FileController.php
@@ -16,12 +16,13 @@
             $name = "tmp/".md5(rand()).time().".jpg";
             if (move_uploaded_file($this->file['tmp_name'], $name)) {
                 $size = filesize($name);      
-                if ($size < $this->maxSize) {   
+                if ($size < $this->maxSize) {    
                     $bytes = addslashes(fread(fopen($name, "r"), $size));
                     shell_exec("rm -rf ".$name);
                     return $bytes;
                 }  else {
                     $this->error = "<p>Tamanho máximo de ".$this->maxSize." bytes</p>";
+                    shell_exec("rm -rf ".$name);
                     return false;
                 }
             }
