5b560e02145d3a66591d7365d9efb5f5682d5cb9
WillGolden80742 <william80742@gmail.com>
2022-08-28 01:26:30 -0300

    filtrando

--- Controller/Message.php
+++ Controller/Message.php
@@ -8,29 +8,77 @@
             $this->msg = str_replace("<", "&lt;",$this->msg);
             $this->msg = str_replace(">", "&gt;",$this->msg);
             $this->msg = str_replace("\"", "&quot;",$this->msg);
+            $msg = $this->msg;
+            if ($this->isYoutube ($msg)) {
+                $msg = $this->youtube ($msg);
+                $msgArray = explode("<style id=\"embed\">",$msg); 
+                $this->msg = $this->link ($msgArray[0])."<style id=\"embed\">".$msgArray[1];
+            } else {
+                $this->msg = $this->link ($this->msg);
+            }
+        }
+
+
+        function link ($text) {
+
+            $urlY1 = "https://";
+            $urlY2 = "http://";
+
+            if (str_contains($text,$urlY1) || str_contains($text,$urlY2)) {
+
+                if (str_contains($text,$urlY2)) {
+                    $id = explode("http://",$text)[1]; 
+                    $id = explode("\n",$id)[0];
+                    $id = explode(" ",$id)[0];
+                    $id = "http://".$id;
+                } else if (str_contains($text,$urlY1))  {
+                    $id = explode("https://",$text)[1];
+                    $id = explode("\n",$id)[0];
+                    $id = explode(" ",$id)[0];
+                    $id = "https://".$id;
+                }
+
+                $text = str_replace($id,"<a href='$id' target=\"_blank\">$id</a>",$text);
+
+            } 
+            return $text;
+        }
+
+        function youtube ($text) {
+
             $urlY1 = "youtube.com/";
             $urlY2 = "youtu.be/";
 
-            if (str_contains($this->msg,$urlY1) || str_contains($this->msg,$urlY2)) {
+            $text = str_replace("&feature=youtu.be", "",$text);
 
-                $this->msg = str_replace("&feature=youtu.be", "",$this->msg);
+            if (str_contains($text,$urlY2)) {
+                $id = explode("youtu.be/",$text)[1]; 
+                $id = explode("\n",$id)[0];
+                $id = explode(" ",$id)[0];
+            } else if (str_contains($text,$urlY1))  {
+                $id=explode("watch?v=",$text)[1];
+                $id = explode("\n",$id)[0];
+                $id = explode(" ",$id)[0];
+            }
 
-                if (str_contains($this->msg,$urlY2)) {
-                    $id = explode("youtu.be/",$this->msg)[1]; 
-                    $id = explode("\n",$id)[0];
-                    $id = explode(" ",$id)[0];
-                } else if (str_contains($this->msg,$urlY1))  {
-                    $id=explode("watch?v=",$this->msg)[1];
-                    $id = explode("\n",$id)[0];
-                    $id = explode(" ",$id)[0];
-                }
+            $text.="<style id=\"embed\"> .thumb-video #$id { background-image:url(\"https://img.youtube.com/vi/".$id."/0.jpg\"); } </style>";
+            $link="<div class='thumb-video' id=\"thumb-video$id\"><center><a href=\"https://youtu.be/".$id."\" target=\"_blank\"><img  id=\"$id\" height=100% src=\"Images/play.svg\"/></a></center></div>";
 
-                $this->msg.="<style> .thumb-video #$id { background-image:url(\"https://img.youtube.com/vi/".$id."/0.jpg\"); } </style>";
-                $link="<div class='thumb-video' id=\"thumb-video$id\"><center><a href=\"https://youtu.be/".$id."\" target=\"_blank\"><img  id=\"$id\" height=100% src=\"Images/play.svg\"/></a></center></div>";
-
-                $this->msg.=$link;
-            }            
+            $text.=$link;
+            
+            return $text;
         }
+
+        function isYoutube ($text) {
+            $urlY1 = "youtube.com/";
+            $urlY2 = "youtu.be/";
+            if (str_contains($text,$urlY1) || str_contains($text,$urlY2)) {
+                return true; 
+            } else {
+                return false;
+            }
+        }        
+
         public function __toString():string {
             return $this->msg;   
         }
