5a1c3ac84ef78e9cbf70ef8f55414c890acf5c4f
WillGolden80742 <william80742@gmail.com>
2022-08-30 08:15:26 -0300

    removendo código não usado

--- Controller/AutenticateController.php
+++ Controller/AutenticateController.php
@@ -111,15 +111,6 @@
 
         function logout () {
             $this->authModel->deleteToken();
-            if (isset($_SERVER['HTTP_COOKIE'])) {
-                $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-                foreach($cookies as $cookie) {
-                    $parts = explode('=', $cookie);
-                    $name = trim($parts[0]);
-                    setcookie($name, '', time()-1000);
-                    setcookie($name, '', time()-1000, '/');
-                }
-            }
             header("Location: login.php");
             die();
         }
