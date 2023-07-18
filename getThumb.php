7b8078a1a6f3b98e8757a0aa92c9fd264c0d6bb6
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2023-07-17 21:33:07 -0300

    Update getThumb.php

--- getThumb.php
+++ getThumb.php
@@ -2,5 +2,5 @@
     session_start();
     include 'Controller/Message.php';
     header("Content-type: application/json; charset=utf-8");
-    echo new Message($_GET['msg'],false);
+    echo new Message($_GET['msg']);
 ?>    
