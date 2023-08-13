9fb34d24edc484bc787f1c541c54e16522106d31
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2023-08-13 20:23:20 -0300

    Update server.php

--- server.php
+++ server.php
@@ -11,6 +11,7 @@ class Chat implements MessageComponentInterface
 {
     protected $clients;
     protected $nickNameMap;
+    protected $messageRateLimit = 3; // Allow 3 messages per second
 
     public function __construct()
     {
@@ -21,37 +22,58 @@ class Chat implements MessageComponentInterface
     public function onOpen(ConnectionInterface $conn)
     {
         $this->clients->attach($conn);
-        echo "Nova conexão! ({$conn->resourceId})\n";
+        echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg)
     {
         $data = json_decode($msg, true);
+        if (!$this->isValidMessage($data)) {
+            return;
+        }
+        
         $nickNameFrom = $data['nickNameFrom'];
         $this->nickNameMap[$nickNameFrom] = $from;
-        if (isset($data['nickNameFrom']) && isset($data['nickNameTo']) && isset($data['message'])) {
-            $nickNameTo = $data['nickNameTo'];
-            $message = $data['message'];
-            if (isset($this->nickNameMap[$nickNameTo])) {
-                $client = $this->nickNameMap[$nickNameTo];
-                $client->send(json_encode([
-                    'from' => $nickNameFrom,
-                    'message' => $message
-                ]));
-            }
+
+        if ($this->isRateLimited($from)) {
+            return;
+        }
+
+        $nickNameTo = $data['nickNameTo'];
+        $message = $data['message'];
+
+        if (isset($this->nickNameMap[$nickNameTo])) {
+            $client = $this->nickNameMap[$nickNameTo];
+            $client->send(json_encode([
+                'from' => $nickNameFrom,
+                'message' => $message
+            ]));
         }
     }
 
     public function onClose(ConnectionInterface $conn)
     {
         $this->clients->detach($conn);
-        echo "Conexão {$conn->resourceId} foi fechada\n";
+        echo "Connection {$conn->resourceId} has been closed\n";
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e)
     {
-        echo "Ocorreu um erro: {$e->getMessage()}\n";
+        echo "An error occurred: {$e->getMessage()}\n";
         $conn->close();
+    }
+
+    protected function isValidMessage($data)
+    {
+        // Implement your own validation logic here
+        return isset($data['nickNameFrom']) && isset($data['nickNameTo']) && isset($data['message']);
+    }
+
+    protected function isRateLimited($conn)
+    {
+        // Implement rate limiting logic here
+        // Track and compare the message rate for each connection
+        return false; // Modify this based on your implementation
     }
 }
 
@@ -64,5 +86,5 @@ $server = IoServer::factory(
     8080
 );
 
-echo "Servidor WebSocket iniciado na porta 8080...\n";
+echo "WebSocket server started on port 8080...\n";
 $server->run();
